a931b784123fff707949f06332f03e5fde9967e4
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2023-12-22 15:56:36 +0700

    update mongodb url

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
     try {
-        await mongoose.connect(process.env.DB);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('connected!!');
     } catch (error) {
         console.log('could not connect to database!!')
