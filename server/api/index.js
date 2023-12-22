6eb2f9ed5a149e8892e7242180762e25f441ff69
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2023-12-22 16:13:30 +0700

    install swagger

--- server/api/index.js
+++ server/api/index.js
@@ -1,22 +1,49 @@
-require("dotenv").config();
-require("express-async-errors");
 const express = require("express");
 const cors = require("cors");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const expressAsyncErrors = require("express-async-errors");
 const connection = require("../db");
 const userRoutes = require("../routes/user");
 const authRoutes = require("../routes/auth");
 const songsRoutes = require("../routes/songs");
 const playlistRoutes = require("../routes/playList");
 const searchRoutes = require("../routes/search");
+
 const app = express();
+
+// Swagger Options
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Lofi-BTB API",
+      version: "1.0.0",
+      description: "API documentation for Lofi-BTB",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+        description: "Development server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Path to the API routes
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 connection();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) =>{
-    res.send('this Lofi-BTB apis')
-})
+app.get("/", (req, res) => {
+  res.send("Lofi-BTB API");
+});
+
+// Serve Swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use("/api/users", userRoutes);
 app.use("/api/login", authRoutes);
 app.use("/api/songs", songsRoutes);
@@ -24,4 +51,6 @@ app.use("/api/playlists", playlistRoutes);
 app.use("/api/", searchRoutes);
 
 const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on pot ${port}...`));
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
