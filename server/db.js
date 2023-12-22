18253294735515c06028f9d004da443327ee0dd8
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2023-12-22 15:49:46 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = async () => {
-    const connectionParams = {
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }
     try {
-        await mongoose.connect(process.env.DB, connectionParams);
+        await mongoose.connect(process.env.DB);
         console.log('connected!!');
     } catch (error) {
         console.log('could not connect to database!!')
